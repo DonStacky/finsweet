a78f723b604b8e338bd719033342bf91a7b57ce2
Сергей (DonStacky) <rodnihsergey1994@mail.ru>
2023-01-04 23:38:46 +0300

    feat: add header

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    $(".rateYo").rateYo({
+      starWidth: "40px"
+    });
+    $(".rateYo").rateYo("option", "starWidth", "36px");
+
+    $(".rateYo").rateYo({
+     normalFill: "#D7D1C7"
+      });
+    $(".rateYo").rateYo("option", "normalFill", "#D7D1C7");
+
+    $(".rateYo").rateYo({
+        ratedFill: "#E74C3C"
+      });
+    $(".rateYo").rateYo("option", "ratedFill", "#FFB648");
+
+
+
+
+
+
+
+
+
+  });
